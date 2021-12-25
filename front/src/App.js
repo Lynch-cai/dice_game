6ca06034fe60068dbce265ab7f74f857ef24805f
Lynch Cai <41620147+Lynch-cai@users.noreply.github.com>
2021-12-25 19:50:40 +0100

    [Feat] Update user

--- front/src/App.js
+++ front/src/App.js
@@ -8,10 +8,32 @@ function App() {
     <div className="App">
       <header className="App-header">
         <GameTable />
-        <User diamond_content="1" first_content="Theotim" second_content="100 points" />
-        <User diamond_content="2" first_content="Theotim" second_content="100 points" />
-        <User diamond_content="3" first_content="Theotim" second_content="100 points" />
-        <User diamond_content="4" first_content="Theotim" second_content="100 points" />
+        <User
+          diamond_content="1"
+          first_content="Theotim"
+          second_content="100 points"
+          in_game="true"
+          turn="true"
+        />
+        <User
+          diamond_content="2"
+          first_content="Theotim"
+          second_content="100 points"
+          in_game="true"
+          turn="false"
+        />
+        <User
+          diamond_content="3"
+          first_content="Theotim"
+          second_content="100 points"
+          in_game="true"
+        />
+        <User
+          diamond_content="4"
+          first_content="Theotim"
+          second_content="100 points"
+          in_game="false"
+        />
         <Button content="Start" type="secondary" />
         <Button content="Start" type="primary" />
       </header>
