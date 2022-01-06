eaf26fed469a2fe69e7bedfd3435ee15986e5fc0
Mehdi <mehdi@ublo.immo>
2022-01-06 12:30:12 +0100

    [Add]: basic routing

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,24 @@
 import './App.css';
 import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import { SettingScreen } from './views/SettingScreen';
+import { StatsScreen } from './views/StatsScreen';
+import { GameScreen } from './views/GameScreen';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/settings" element={<SettingScreen />} />
+          <Route path="/fun" element={<GameScreen />} />
+          <Route path="/stats" element={<StatsScreen />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
