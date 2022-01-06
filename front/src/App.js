1d280d0bc52f4338d991eb4919a465a4ef5404f4
Mehdi <mehdi@ublo.immo>
2022-01-06 17:10:04 +0100

    [Add]: a new NotFoundScreen component witch redirect automatically if the path does not exist

--- front/src/App.js
+++ front/src/App.js
@@ -2,9 +2,10 @@ import './App.css';
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import { SettingScreen } from './views/SettingScreen';
-import { StatsScreen } from './views/StatsScreen';
 import { GameScreen } from './views/GameScreen';
+import { StatsScreen } from './views/StatsScreen';
+import { SettingScreen } from './views/SettingScreen';
+import { NotFoundScreen } from './views/NotFoundScreen';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/settings" element={<SettingScreen />} />
           <Route path="/fun" element={<GameScreen />} />
           <Route path="/stats" element={<StatsScreen />} />
+          <Route path="*" element={<NotFoundScreen />} />
         </Routes>
       </BrowserRouter>
     </div>
